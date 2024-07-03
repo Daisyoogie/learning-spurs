081908c48f3da7e7a33d68fbce4feb878d5f1485
Kevin Smith <ks24175@gmail.com>
2024-07-03 10:38:01 +0100

    Initial commit: this is a long time coming.
    I have been working on git for a while. Make sure the files are easy to find.

---
+++ Tottenham/spurs/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+import Header from './Header';
+
+
+function App() {
+  return (
+    <div className="app">   
+
+      {/* header */}    
+        <Header />
+
+      {/* App Body */}  
+
+        {/* Sidebar */}   
+
+          {/* Feed */}   
+              {/* Widget*/}  
+    </div>
+  );
+}
+
+export default App;
